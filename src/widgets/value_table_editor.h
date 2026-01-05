117a54b1261dc5d80513ff0c39b1a50f56d24a9a
Dean Lee <deanlee3@gmail.com>
2026-01-05 14:36:41 +0800

    Move ValueDescriptionDialog  to standalone ValueTableEditor widget (#31)

---
+++ src/widgets/value_table_editor.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <QDialog>
+#include <QTableWidget>
+#include "dbc/dbc.h"
+#include <QStyledItemDelegate>
+
+class ValueTableEditor : public QDialog {
+ public:
+  ValueTableEditor(const ValueDescription& descriptions, QWidget* parent);
+  ValueDescription val_desc;
+
+ private:
+  struct Delegate : public QStyledItemDelegate {
+    Delegate(QWidget* parent) : QStyledItemDelegate(parent) {}
+    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+  };
+
+  void save();
+  QTableWidget* table;
+};
