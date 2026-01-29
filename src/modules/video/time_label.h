4bbecaabf65b0a0daa6629735134d201e97a145f
Dean Lee <deanlee3@gmail.com>
2026-01-30 03:46:35 +0800

    ux: bold current timestamp in playback control for better visual focus (#381)

--- src/modules/video/time_label.h
+++ src/modules/video/time_label.h
@@ -1,20 +1,25 @@
 #pragma once
+
+#include <QFont>
 #include <QWidget>
 
 class TimeLabel : public QWidget {
   Q_OBJECT
  public:
   TimeLabel(QWidget* parent = nullptr);
-
-  void setText(const QString& new_text);
-
-  QString text;
- signals:
-  void clicked();
+  void setTime(double cur, double total = -1);
 
  protected:
+  QString formatTime(double sec, bool include_milliseconds = false);
+  void updateTime();
   void paintEvent(QPaintEvent* event) override;
-  void mousePressEvent(QMouseEvent* event) override {
-    emit clicked();
-  }
+  void mousePressEvent(QMouseEvent* event) override;
+
+  double current_sec = 0;
+  double total_sec = -1;
+  QString current_sec_text;
+  QString total_sec_text;
+  QFont fixed_font;
+  QFont bold_font;
+  int cur_time_width = 0;
 };
