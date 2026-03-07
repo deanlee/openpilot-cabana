9dcb915ca1cbe6956b297ca12ef2c0cd2bc473a4
Dean Lee <deanlee3@gmail.com>
2026-03-08 04:05:58 +0800

    improve SocketCanWidget (#495)

--- src/modules/streams/socketcan.cc
+++ src/modules/streams/socketcan.cc
@@ -14,11 +14,9 @@ SocketCanWidget::SocketCanWidget(QWidget* parent) : AbstractStreamWidget(parent)
 
   QHBoxLayout* device_layout = new QHBoxLayout();
   device_edit = new QComboBox();
-  device_edit->setFixedWidth(300);
-  device_layout->addWidget(device_edit);
+  device_layout->addWidget(device_edit, 1);
 
   QPushButton* refresh = new QPushButton(tr("Refresh"));
-  refresh->setFixedWidth(100);
   device_layout->addWidget(refresh);
   form_layout->addRow(tr("Device"), device_layout);
   main_layout->addLayout(form_layout);
@@ -27,17 +25,18 @@ SocketCanWidget::SocketCanWidget(QWidget* parent) : AbstractStreamWidget(parent)
   setFocusProxy(device_edit);
 
   connect(refresh, &QPushButton::clicked, this, &SocketCanWidget::refreshDevices);
-  connect(device_edit, &QComboBox::currentTextChanged, this, [=] { config.device = device_edit->currentText(); });
+  connect(device_edit, &QComboBox::currentTextChanged, this, [this](const QString& text) { config.device = text; });
 
-  // Populate devices
   refreshDevices();
 }
 
 void SocketCanWidget::refreshDevices() {
   device_edit->clear();
-  for (auto device : QCanBus::instance()->availableDevices(QStringLiteral("socketcan"))) {
+  for (auto& device : QCanBus::instance()->availableDevices(QStringLiteral("socketcan"))) {
     device_edit->addItem(device.name());
   }
+  config.device = device_edit->currentText();
+  emit enableOpenButton(!config.device.isEmpty());
 }
 
 AbstractStream* SocketCanWidget::open() {
